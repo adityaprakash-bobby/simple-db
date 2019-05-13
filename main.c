8b425778fd27a853d85b36e719618ac77fa907ec
Aditya <adityaprakash.bobby@gmail.com>
2019-05-13 13:20:40 +0530

    Modularize and other changes

--- main.c
+++ main.c
@@ -1,238 +1,8 @@
-/*
-    Basic REPL structure for the DB
-*/
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-#define TABLE_MAX_PAGES 100
-
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
-/*
-    Enumerators to handle various status codes related to the different
-    funtions and implementations
-*/
-enum MetaCommandResult_t {
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-};
-
-typedef enum MetaCommandResult_t MetaCommandResult;
-
-enum PrepareResult_t {
-    PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT
-};
-
-typedef enum PrepareResult_t PrepareResult;
-
-enum StatementType_t {
-    STATEMENT_INSERT,
-    STATEMENT_SELECT
-};
-
-typedef enum StatementType_t StatementType;
-
-struct InputBuffer_t {
-
-    char* buffer;
-    size_t buffer_length;
-    ssize_t input_length;
-
-};
-
-typedef struct InputBuffer_t InputBuffer;
-
-// A row data structure to store the data into a hard coded table
-struct Row_t {
-
-    uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
-
-};
-
-typedef struct Row_t Row;
-
-struct Statement_t {
-  
-    StatementType type;
-    Row row_to_insert;
-
-};
-
-typedef struct Statement_t Statement;
-
-// Structure to keep track of the pages of the rows
-struct Table_t {
-
-    uint32_t num_rows;
-    void* pages[TABLE_MAX_PAGES]
-
-};
-
-typedef struct Table_t Table;
-
-// A small wrapper to interract with getline()
-InputBuffer* new_input_buffer() {
-    
-    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-    
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
-
-    return input_buffer;
-
-}
-
-// Compact representation of a Row in the table
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
-// Keeping track of the number of rows
-const uint32_t PAGE_SIZE = 4096;
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-
-// Print a prompt for the user
-void print_prompt() {
-    printf("db>");
-}
-
-// Read input from STDIN
-void read_input(InputBuffer* input_buffer) {
-
-    ssize_t bytes_read = getline(&(input_buffer->buffer), 
-                            &(input_buffer->buffer_length), stdin);
-
-    if (bytes_read <= 0) {
-        
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
-
-    }
-
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
-}
-
-// Function to free allocated memory for the instance of InputBuffer
-void close_input_buffer(InputBuffer* input_buffer) {
-
-    free(input_buffer->buffer);
-    free(input_buffer);
-
-}
-
-// Wrapper that handles non-SQL commands like '.exit' and leaves room for more 
-// such commands
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
-
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        exit(EXIT_SUCCESS);
-    }   
-
-    else {
-        return META_COMMAND_UNRECOGNIZED_COMMAND;
-    }
-
-}
-
-// The SQL compiler
-PrepareResult prepare_statement(InputBuffer* input_buffer, 
-                                Statement* statement) {
-    
-    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-                input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned > 3)
-            return PREPARE_SYNTAX_ERROR;
-
-        return PREPARE_SUCCESS;
-
-    }
-
-    if (strcmp(input_buffer->buffer, "select") == 0) {
-
-        statement->type = STATEMENT_SELECT;
-        return PREPARE_SUCCESS;
-
-    }
-
-    return PREPARE_UNRECOGNIZED_STATEMENT;
-
-}
-
-void execute_statement(Statement* statement) {
-
-    switch(statement->type) {
-
-        case (STATEMENT_INSERT):
-            printf("Insert funtion will be handled here.\n");
-            break;
-
-        case (STATEMENT_SELECT):
-            printf("Select funtion will be handled here.\n");
-            break;
-
-    }
-
-}
-
-// Function to serialize the row data 
-void serialize_row(Row* source, void* destination) {
-
-    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
-
-}
-
-// Function to deserialize the row data from memory
-void deserialize_row(void* source, Row* destination) {
-
-    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
-    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);    
-
-}
-
-// Memory page for reading and writing
-void* row_slot(Table* table, uint32_t row_num) {
-
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = table->pages[page_num];
-
-    if (page == NULL) {
-
-        // Allocate memory only when we try to access page
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-
-    }
-
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-    return page + byte_offset;
-
-}
+# include "db.h"
 
 int main (int argc, char *argv[]) {
 
+    Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
 
     while(true) {
@@ -260,6 +30,11 @@ int main (int argc, char *argv[]) {
 
             case (PREPARE_SUCCESS):
                 break;
+
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error. Coul not parse statement.\n");
+                continue;
+
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword as start of '%s'.\n", 
                         input_buffer->buffer);
@@ -267,8 +42,17 @@ int main (int argc, char *argv[]) {
 
         }
 
-        execute_statement(&statement);
-        printf("Executed.\n");
+        switch (execute_statement(&statement, table)) {
+
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n")
+                break;
+
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: Table full.\n");
+                break;
+
+        }
 
     }
 
