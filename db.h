e90a23010017b21028b72ca40aecd8f377189fad
Aditya <adityaprakash.bobby@gmail.com>
2019-05-15 15:51:04 +0530

    Added persistence to the database; Create db files, read them, write to them;

--- db.h
+++ db.h
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <errno.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
@@ -147,6 +150,42 @@ void print_row(Row* row) {
     printf("( %d, %s, %s )\n", row->id, row->username, row->email);
 } 
 
+// Get a page and handle a cache miss
+void* get_page(Pager* pager, uint32_t page_num) {
+
+    if (page_num > TABLE_MAX_PAGES) {
+        printf("Tried to fetch page number out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pager->pages[page_num] == NULL) {
+        // A cache miss. Allocate memory and load from file.
+        void *page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+ 
+        // We might save a partial page at the end of the file
+        if (pager->file_length % PAGE_SIZE) {
+            num_pages += 1;
+        }
+
+        if (page_num <= num_pages) {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+        
+            if (bytes_read == -1) {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    
+    }
+
+    return pager->pages[page_num];
+
+}
+
 // Memory page for reading and writing
 void* row_slot(Table* table, uint32_t row_num) {
 
@@ -189,11 +228,103 @@ void close_input_buffer(InputBuffer* input_buffer) {
 
 }
 
+// Function to flush the data at the end of the db file
+void* pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+
+    if (pager->pages[page_num] == NULL) {
+
+        printf("Tried to flush null page.\n");
+        exit(EXIT_FAILURE);
+
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if (offset == -1) {
+
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+
+    }
+
+    ssize_t bytes_written = 
+        write(pager->file_descriptor, pager->pages[page_num], size);
+    
+    if (bytes_written == -1) {
+
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    
+    }
+
+}
+
+// Flush page cache to disk, close the file, free memory for
+// the pager and Table data structures
+void db_close(Table* table) {
+    
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++) {
+
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+
+    }
+    
+    // There may be a partial page to write to the end of this file
+    // This would not be needed after we switch to a B-Tree
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+
+        uint32_t page_num = num_full_pages;
+
+        if (pager->pages[page_num] != NULL) {
+
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+
+        }
+
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1) {
+
+        printf("Error closing the db file.\n");
+        exit(EXIT_FAILURE);
+
+    }
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+
+        void* page = pager->pages[i];
+        if (page) {
+
+            free(page);
+            pager->pages[i] = NULL;
+        
+        }
+    
+    }
+
+    free(pager);
+    free(table);
+}
+
 // Wrapper that handles non-SQL commands like '.exit' and leaves room for more 
 // such commands
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
 
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        db_close(table);
         exit(EXIT_SUCCESS);
     }   
 
@@ -257,22 +388,6 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
 
 }
 
-// void execute_statement(Statement* statement) {
-
-//     switch(statement->type) {
-
-//         case (STATEMENT_INSERT):
-//             printf("Insert funtion will be handled here.\n");
-//             break;
-
-//         case (STATEMENT_SELECT):
-//             printf("Select funtion will be handled here.\n");
-//             break;
-
-//     }
-
-// }
-
 ExecuteResult execute_insert(Statement* statement, Table* table) {
 
     if (table->num_rows >= TABLE_MAX_ROWS) {
@@ -319,8 +434,8 @@ Pager* pager_open(const char* filename) {
 
     int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
 
-    if (fd = -1) {
-        printf("Unable to open file,\n");
+    if (fd == -1) {
+        printf("Unable to open file.\n");
         exit(EXIT_FAILURE);
     }
 
@@ -349,17 +464,5 @@ Table* db_open(const char* filename) {
     table->num_rows = num_rows;
 
     return table;
-
-}
-
-void free_table(Table* table) {
-
-    for (int i = 0; table->pages[i]; i++) {
-
-        free(table->pages[i]);
-
-    }
-
-    free(table);
 
 }
