9f0764b3acc840bde6449f88a50e14ed65d6bc88
Aditya <adityaprakash.bobby@gmail.com>
2019-05-17 18:58:36 +0530

    Set node type and handle dulpicate keys

--- db.h
+++ db.h
@@ -136,6 +136,26 @@ const uint32_t LEAF_NODE_SPACE_FOR_CELLS =
 const uint32_t LEAF_NODE_MAX_CELLS = 
                 LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
+/* 
+    Function to get the NODE type
+*/
+NodeType get_node_type(void* node) {
+
+    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType)value;
+
+}
+
+/*
+    Function to set the NODE type
+*/
+void set_node_type(void* node, NodeType type) {
+
+    uint8_t value = type;
+    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+
+}
+
 /*
     Accessing Leaf Nodes
 */
@@ -156,6 +176,7 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
 }
 
 void initialize_leaf_node(void* node) {
+    set_node_type(node, NODE_LEAF);
     *leaf_node_num_cells(node) = 0;
 }
 
--- main.c
+++ main.c
@@ -77,6 +77,10 @@ int main (int argc, char *argv[]) {
                 printf("Executed.\n");
                 break;
 
+            case (EXECUTE_DUPLICATE_KEY):
+                printf("Error: Duplicate key.\n");
+                break;
+
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
                 break;
